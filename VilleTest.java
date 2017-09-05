3eb4c38b688c683082872fb575b1f39443d670e2
thieux <mathieu.pauly@gmail.com>
2017-09-05 22:26:28 +0200

    ''

--- VilleTest.java
+++ VilleTest.java
@@ -55,7 +55,7 @@ public class VilleTest {
                 System.out.println("A");
             } else if (Integer.parseInt(column) > 500_000) {
                 System.out.println("B");
-            }else if (Integer.parseInt(column) > 500_000) {
+            }else if (Integer.parseInt(column) > 200_000) {
                 System.out.println("B");
             }
         }
