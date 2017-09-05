6159630ea4a4ec9dfbeeb079f7ef0d5d2bb864d9
thieux <mathieu.pauly@gmail.com>
2017-09-05 22:29:24 +0200

    ''

--- VilleTest.java
+++ VilleTest.java
@@ -55,7 +55,7 @@ public class VilleTest {
             if (Integer.parseInt(column) > 1_000_000) {
                 countA++;
             } else if (Integer.parseInt(column) > 500_000) {
-                countA++;
+                countB++;
                 System.out.println("B");
             } else if (Integer.parseInt(column) > 200_000) {
                 System.out.println("C");
