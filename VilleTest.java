536e22af72e2297acb77a79894de3d0bac6dbebe
thieux <mathieu.pauly@gmail.com>
2017-09-05 22:39:53 +0200

    ''

--- VilleTest.java
+++ VilleTest.java
@@ -62,7 +62,7 @@ public class VilleTest {
                 Integer a = map.getOrDefault("C", 0);
                 map.put("C", a + 1);
             } else if (Integer.parseInt(column) > 100_000) {
-                Integer a = map.getOrDefault("C", 0);
+                Integer a = map.getOrDefault("D", 0);
                 map.put("C", a + 1);
             }
         }
