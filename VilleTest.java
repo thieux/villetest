3ef680489e334a68dfb246063a43861b5e1bc805
thieux <mathieu.pauly@gmail.com>
2017-09-05 22:38:44 +0200

    ''

--- VilleTest.java
+++ VilleTest.java
@@ -46,7 +46,6 @@ public class VilleTest {
         String[] split = data.split("\n");
         System.out.println(Arrays.toString(split));
 
-        int countB = 0;
         int countC = 0;
         int countD = 0;
         HashMap<String, Integer> map = new HashMap<>();
