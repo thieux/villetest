12fed8f49f07d65bd688a923d0c0118d05684291
thieux <mathieu.pauly@gmail.com>
2017-09-05 22:39:21 +0200

    ''

--- VilleTest.java
+++ VilleTest.java
@@ -61,7 +61,7 @@ public class VilleTest {
                 map.put("B", a + 1);
             } else if (Integer.parseInt(column) > 200_000) {
                 Integer a = map.getOrDefault("C", 0);
-                map.put("B", a + 1);
+                map.put("C", a + 1);
             } else if (Integer.parseInt(column) > 100_000) {
                 countD++;
             }
