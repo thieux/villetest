e02183bc97c95b7ff176b204de166c1dc0b3c206
thieux <mathieu.pauly@gmail.com>
2017-09-05 22:01:39 +0200

    ''

--- VilleTest.java
+++ VilleTest.java
@@ -1,5 +1,21 @@
 package fr.arolla.java8esgi.partiel;
 
-public class VilleTest {
+import org.junit.Test;
 
+public class VilleTest {
+    @Test
+    public void name() throws Exception {
+        String data = "1\t75\tParis\t2181371\n" +
+                "2\t13\tMarseille\t839043\n" +
+                "3\t69\tLyon\t472305\n" +
+                "4\t31\tToulouse\t437715\n" +
+                "5\t06\tNice\t347060\n" +
+                "6\t44\tNantes\t282853\n" +
+                "7\t67\tStrasbourg\t272975\n" +
+                "8\t34\tMontpellier\t251634\n" +
+                "9\t33\tBordeaux\t232260\n" +
+                "10\t59\tLille\t226014\n" +
+                "11\t35\tRennes\t209613\n" +
+                "12\t51\tReims\t183837\n" +
+    }
 }
