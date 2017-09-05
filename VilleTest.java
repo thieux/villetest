9499e6fdfca00b9e4e38f4ef25a76cc22c6997f0
thieux <mathieu.pauly@gmail.com>
2017-09-05 22:08:10 +0200

    ''

--- VilleTest.java
+++ VilleTest.java
@@ -41,7 +41,7 @@ public class VilleTest {
     private void printCityClasses(String data) {
         System.out.printf("%s: %4d%n", "A", 1);
         System.out.printf("%s: %4d%n", "A", 1);
-        System.out.println("B: 1");
+        System.out.println("B: ");
         System.out.println("C: 9");
         System.out.println("D: 14");
     }
